df4435a7b583c6a6e3659e3f1e3ef31f91b4ef89
Kurnia Kharisma <kurniakharisma17@gmail.com>
2022-10-31 10:45:35 +0700

    adding logout button and fixing bugs on login

--- src/App.js
+++ src/App.js
@@ -2,16 +2,21 @@ import React, { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Login from './pages/Login';
 import Register from './pages/Register';
+import {FiLogOut} from 'react-icons/fi';
 import { getUserLogged, putAccessToken } from './utils/network-data';
 
 export default function App() {
     const [auth, setAuth] = useState();
     const [loading, setLoading] = useState();
+    const [initializing, setInitializing] = useState(true);
 
     useEffect(()=>{
         getUserLogged().then(({error, data})=>{
             if(!error){
                 setAuth(data);
+                setInitializing(false);
+            }else{
+                setInitializing(false);
             }
         })
     },[])
@@ -19,22 +24,35 @@ export default function App() {
     async function LoginSucces({accessToken}){
         putAccessToken(accessToken);
         const {data} = await getUserLogged();
-
         setAuth(data);
     }
 
-    return (
-        <div>
-            {!auth && 
-            <Routes>
-                <Route path='/*' element={<Login LoginSucces={LoginSucces}></Login>}/>
-                <Route path='/register' element={<Register></Register>}/>
-            </Routes>
+    function onLogoutHandler(){
+        setAuth(()=>{
+            return null
+        })
+        putAccessToken('')
+    }
+
+    return (<>
+            {initializing ? null :
+            <div>
+                {!auth && 
+                <Routes>
+                    <Route path='/*' element={<Login LoginSucces={LoginSucces}></Login>}/>
+                    <Route path='/register' element={<Register></Register>}/>
+                </Routes>
+                }
+                {
+                    auth && 
+                    <>
+                        <p>Welcome! {auth.name}</p>
+                        <FiLogOut onClick={onLogoutHandler}></FiLogOut>
+                    </>
+                    
+                }
+            </div>
             }
-            {
-                auth && 
-                <p>Welcome! {auth.name}</p>
-            }
-        </div>
+        </>
     )
 }
